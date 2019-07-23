1b8e85971816ab8c46726a298779155a35d2fb82
Chris Hodge <chrishodge87@gmail.com>
2019-07-23 09:55:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,7 +17,11 @@ game_hash = {
         :team_name => "Charlotte Hornets"
         :colors    => ["Turquoise", "Purple"]
         :players   => [
-    {"Alan And"=>{:number=>0,:shoe=>16,:points=>22,:rebounds=>12,:assists=>12,:steals=>3,:blocks=>1,:slam_dunks=>1}},  
+    {"Jeff Adrien"=>{:number=>4,:shoe=>18,:points=>10,:rebounds=>1,:assists=>1,:steals=>2,:blocks=>7,:slam_dunks=>2}},
+    {"Bismack Biyombo"=>{:number=>0,:shoe=>16,:points=>12,:rebounds=>4,:assists=>7,:steals=>4,:blocks=>15,:slam_dunks=>10}},
+    {"DeSagna Diop"=>{:number=>2,:shoe=>14,:points=>24,:rebounds=>12,:assists=>12,:steals=>4,:blocks=>5,:slam_dunks=>5}},
+    {"Ben Gordon"=>{:number=>8,:shoe=>15,:points=>33,:rebounds=>3,:assists=>2,:steals=>1,:blocks=>1,:slam_dunks=>0}},
+    {"Kemba Walker"=>{:number=>33,:shoe=>15,:points=>6,:rebounds=>12,:assists=>12,:steals=>7,:blocks=>5,:slam_dunks=>12}},
     ]
   }
 
