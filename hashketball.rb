291ed0cf32e47ea67e373c2c4f2bce552b98190a
Chris Hodge <chrishodge87@gmail.com>
2019-07-23 09:56:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,7 @@ game_hash = {
     {"Manson Plumlee"=>{:number=>1,:shoe=>19,:points=>26,:rebounds=>11,:assists=>6,:steals=>3,:blocks=>8,:slam_dunks=>5}},
     {"Jason Terry"=>{:number=>31,:shoe=>15,:points=>19,:rebounds=>2,:assists=>2,:steals=>4,:blocks=>11,:slam_dunks=>1}}
                           ]
-            }
+            },
   :away => {
         :team_name => "Charlotte Hornets",
         :colors    => ["Turquoise", "Purple"],
